f7e560e73055bbf8776fd28e2d3de74acbad42be
Thiago Lorente Kraetzer <thlindustries23@gmail.com>
2022-11-11 18:13:09 -0300

    Update api.ts

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://stoompizza-server.herokuapp.com/',
+  baseURL: 'https://stoompizza-server-production.up.railway.app/',
 });
 
 export default api;
